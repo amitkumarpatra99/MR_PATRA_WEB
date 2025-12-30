9882fd0d95f8fe3891cf180d6549922e95726451
AMIT KUMAR PATRA <amitkumarpatraaa@hotmail.com>
2025-12-30 14:43:27 +0530

    refactor: Remove Lenis smooth scrolling implementation from App and CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import Lenis from 'lenis';
+import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import PatraAI from "./components/PatraAI/PatraAI";
 import Home from "./components/Home/Home";
@@ -21,25 +20,6 @@ import ContactPage from "./pages/ContactPage";
 import { ThemeProvider } from "./context/ThemeContext";
 
 const App = () => {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.5,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smooth: true,
-      smoothTouch: false, // Optional: normally false for better native feel on touch but can be true
-    })
-
-    function raf(time) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-
-    requestAnimationFrame(raf)
-
-    return () => {
-      lenis.destroy()
-    }
-  }, [])
 
   return (
     <ThemeProvider>
